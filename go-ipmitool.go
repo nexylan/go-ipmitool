6b7c4542997582e5f8eb96d1799ec270247a5367
Henri Devigne <henri.devigne@bonkgaming.fr>
2019-06-06 17:32:53 +0200

    create init function to verify package existence

--- go-ipmitool.go
+++ go-ipmitool.go
@@ -1,1 +1,13 @@
 package go_ipmitool
+
+import (
+	"log"
+	"os/exec"
+)
+
+func init() {
+	_ , err := exec.LookPath("ipmitool")
+	if err != nil {
+		log.Fatal("You must install ipmitool before use this package")
+	}
+}
